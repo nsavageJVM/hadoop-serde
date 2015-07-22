6d3d7563909b1ac23c4c7cff1278d11a233e9b8c
nsavagejvm <nsavagejvm@eyahoo.com>
2015-07-22 18:44:43 +0300

    update

--- src/main/java/com/eduonix/hadoop/partone/pig/PigETL.java
+++ src/main/java/com/eduonix/hadoop/partone/pig/PigETL.java
@@ -9,6 +9,8 @@ import java.io.IOException;
  * https://cwiki.apache.org/confluence/display/PIG/AvroStorage
  * http://hortonworks.com/blog/pig-as-connector-part-one-pig-mongodb-and-node-js/
  *
+ * http://chimera.labs.oreilly.com/books/1234000001811/ch05.html#filter
+ *
  */
 public class PigETL {
 
@@ -16,6 +18,7 @@ public class PigETL {
 
     private static  String projectRootPath = System.getProperty("user.dir");
 
+    private static String regex = "'.*please.*'";
 
 
 
@@ -26,42 +29,60 @@ public class PigETL {
         }
 
 //
-//        String jarPathUDF =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "pigmodule.jar");
-//        String jarPathArvo =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "avro-1.7.5.jar");
-//        String jarPathJson =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "json-simple-1.1.jar");
-//        String jarPathMon =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "mongo-java-driver-3.0.0.jar");
-//        String jarPathMonH =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "mongo-hadoop-core-1.4.1-SNAPSHOT.jar");
-//        String jarPathMonP =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "mongo-hadoop-pig-1.4.1-SNAPSHOT.jar");
-//        String jarPathJm =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "jackson-mapper-asl-1.9.13.jar");
-//        String jarPathJc =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "jackson-core-asl-1.9.13.jar");
+        String jarPathUDF =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "pigmodule.jar");
+        String jarPathArvo =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "avro-1.7.5.jar");
+        String jarPathJson =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "json-simple-1.1.jar");
+        String jarPathMon =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "mongo-java-driver-3.0.0.jar");
+        String jarPathMonH =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "mongo-hadoop-core-1.4.1-SNAPSHOT.jar");
+        String jarPathMonP =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "mongo-hadoop-pig-1.4.1-SNAPSHOT.jar");
+        String jarPathJm =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "jackson-mapper-asl-1.9.13.jar");
+        String jarPathJc =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "jackson-core-asl-1.9.13.jar");
 
         String jarPathPiggy =  String.format("%s/%s/%s",projectRootPath, "pig-jars"  , "piggybank-0.14.0.jar");
 
         // run pig in local mode
             pigServer = new PigServer(ex);
        try {
-             pigServer.registerJar(jarPathPiggy);
-//
-////            pigServer.registerJar(jarPathUDF);
-////            pigServer.registerJar(jarPathArvo);
-////            pigServer.registerJar(jarPathJson);
-////            pigServer.registerJar(jarPathMon);
-////            pigServer.registerJar(jarPathMonH);
-////            pigServer.registerJar(jarPathMonP);
-////            pigServer.registerJar(jarPathJm);
-////            pigServer.registerJar(jarPathJc);
-//
+
+            pigServer.registerJar(jarPathPiggy);
+            pigServer.registerJar(jarPathUDF);
+            pigServer.registerJar(jarPathArvo);
+            pigServer.registerJar(jarPathJson);
+            pigServer.registerJar(jarPathMon);
+            pigServer.registerJar(jarPathMonH);
+            pigServer.registerJar(jarPathMonP);
+            pigServer.registerJar(jarPathJm);
+            pigServer.registerJar(jarPathJc);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-     //   pigServer.registerQuery("avros = load 'ser_twitter.avro' using  org.apache.pig.piggybank.storage.avro.AvroStorage();");
+    //   pigServer.registerQuery("avros = load './ser_twitter.avro' using  org.apache.pig.piggybank.storage.avro.AvroStorage();");
+//
+//
+//        pigServer.registerQuery("raw = load 'raw_data.txt' USING TextLoader AS (line:chararray);");
+//        pigServer.registerQuery("raw_limit_5 = LIMIT raw 50;");
+//
+//        pigServer.registerQuery("raw_limit_5 = LIMIT raw 50;");
+//
+//        pigServer.registerQuery("B = FOREACH raw_limit_5 GENERATE $0;");
 
-    //    pigServer.registerQuery("STORE avros INTO '$home/testOut/testCnt' USING AvroStorage ();");
+        pigServer.registerQuery("raw = load 'raw_data.txt' as (str:chararray);");
 
-        pigServer.registerQuery("raw = load 'raw_data.txt' ;");
-        pigServer.registerQuery("warns = FILTER raw BY $0 MATCHES '*RT*';");
-        pigServer.registerQuery("STORE warns into '/root/pig_out';");
+        pigServer.registerQuery("C = filter raw by str matches '.*please.*';" );
+
+
+        if (!isLocal) {
+            pigServer.registerQuery("STORE C  into '/root/pig_out';");
+        } else {
+
+            pigServer.registerQuery("STORE C into './pig_out/first_collection.json' USING JsonStorage();");
+
+
+
+        }
+
 
 
    //     pigServer.registerQuery("avros = store avros into 'mongodb://localhost:27017/twitts' using com.mongodb.hadoop.pig.MongoInsertStorage;");
